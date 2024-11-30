9b6b5c61266145ed963414832c80980cedb0c919
IlonaWishec <ilonawishec.management@gmail.com>
2024-11-30 17:25:33 +0300

    module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -20,6 +20,10 @@ info = [
 result = custom_write('test.txt', info)
 for elem in result.items():
   print(elem)
+#  ((1, 0), 'Text for tell.')
+#  ((2, 16), 'Используйте кодировку utf-8.')
+#  ((3, 66), 'Because there are 2 languages!')
+#  ((4, 98), 'Спасибо!')
 
 
 
